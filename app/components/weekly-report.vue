60087b540e21b5d7b75703e064f527c902a608d9
Brett <brettatoms@gmail.com>
2017-07-19 14:22:49 -0400

    add average distance

--- app/components/weekly-report.vue
+++ app/components/weekly-report.vue
@@ -4,6 +4,7 @@
             <th>Week</th>
             <th>Total jogs</th>
             <th>Total distance</th>
+            <th>Avg distance</th>
             <th>Avg. speed</th>
         </thead>
         <tbody>
@@ -11,6 +12,7 @@
                 <td>{{ row.startOfWeek }}</td>
                 <td>{{ row.numJogs }}</td>
                 <td>{{ row.totalDistance }} ft.</td>
+                <td>{{ row.averageDistance }} ft.</td>
                 <td>{{ row.averageSpeed }} mph</td>
             </tr>
         </tbody>
@@ -23,19 +25,15 @@
  export default {
      computed: {
          formattedData() {
-             console.log(this.$store.state.jogs)
              const totals = this.$store.state.jogs.reduce((groups, jog) => {
                  const startOfWeek = moment(jog.date).startOf('week').format('YYYY-MM-DD')
-                 /* const averageSpeed = ((jog.distance_in_feet / 5280 ) / (jog.time_in_seconds))*/
                  groups[startOfWeek] = {
-                     /* averageSpeed: get(groups, `${startOfWeek}.averageSpeed`, 0) + averageSpeed*/
                      timeInSeconds: get(groups, `${startOfWeek}.timeInSeconds`, 0) + jog.time_in_seconds,
                      distanceInFeet: get(groups, `${startOfWeek}.distanceInFeet`, 0) + jog.distance_in_feet,
                      numJogs: get(groups, `${startOfWeek}.numJogs`, 0) + 1
                  }
                  return groups
              }, {})
-             console.log(totals)
 
              const sortedWeeks = Object.keys(totals).sort()
 
@@ -45,11 +43,11 @@
                  return {
                      startOfWeek,
                      numJogs: numJogs,
+                     totalDistance: distanceInFeet,
+                     averageDistance: Math.round(distanceInFeet / numJogs),
                      averageSpeed: averageSpeed.toFixed(2),
-                     totalDistance: distanceInFeet
                  }
              })
-             console.log(weeklyAverages)
 
              return weeklyAverages
          }
